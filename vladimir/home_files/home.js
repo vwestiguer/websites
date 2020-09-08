3ed13eb409a55d48b4a0fdd0bbe714969499f65d
Vladimir Wischansky <70922511+vwestiguer@users.noreply.github.com>
2020-09-08 18:35:52 -0300

    Identaçao
    identacao

--- vladimir/home_files/home.js
+++ vladimir/home_files/home.js
@@ -1,11 +1,9 @@
 // Created by iWeb 3.0.4 local-build-20120420
 
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_3:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#463c3c',opacity:0.800000}),shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#463c3c',opacity:0.800000}),shadow_5:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#463c3c',opacity:0.800000}),shadow_2:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#463c3c',opacity:0.800000}),shadow_6:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#463c3c',opacity:0.800000}),shadow_4:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#463c3c',opacity:0.800000}),shadow_1:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#463c3c',opacity:0.750000})});registry.applyEffects();}
-function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('home_files/homeMoz.css')
-Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
-function onPageUnload()
-{Widget.onunload();}
+setTransparentGifURL('Media/transparent.gif'); function applyEffects() { var registry = IWCreateEffectRegistry(); registry.registerEffects({ shadow_3: new IWShadow({ blurRadius: 4, offset: new IWPoint(1.4142, 1.4142), color: '#463c3c', opacity: 0.800000 }), shadow_0: new IWShadow({ blurRadius: 4, offset: new IWPoint(1.4142, 1.4142), color: '#463c3c', opacity: 0.800000 }), shadow_5: new IWShadow({ blurRadius: 4, offset: new IWPoint(1.4142, 1.4142), color: '#463c3c', opacity: 0.800000 }), shadow_2: new IWShadow({ blurRadius: 4, offset: new IWPoint(1.4142, 1.4142), color: '#463c3c', opacity: 0.800000 }), shadow_6: new IWShadow({ blurRadius: 4, offset: new IWPoint(1.4142, 1.4142), color: '#463c3c', opacity: 0.800000 }), shadow_4: new IWShadow({ blurRadius: 4, offset: new IWPoint(1.4142, 1.4142), color: '#463c3c', opacity: 0.800000 }), shadow_1: new IWShadow({ blurRadius: 4, offset: new IWPoint(1.4142, 1.4142), color: '#463c3c', opacity: 0.750000 }) }); registry.applyEffects(); }
+function hostedOnDM() { return false; }
+function onPageLoad() {
+    loadMozillaCSS('home_files/homeMoz.css')
+    Widget.onload(); fixupAllIEPNGBGs(); fixAllIEPNGs('Media/transparent.gif'); applyEffects()
+}
+function onPageUnload() { Widget.onunload(); }
